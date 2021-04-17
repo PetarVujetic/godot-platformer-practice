770d3dc7290abcaeb7d084d90f2c1a25ac4fba0c
PetarVujetic <97petar@live.com>
2021-04-17 15:21:15 +0200

    first level

---
+++ src/Actors/Player.gd
@@ -0,0 +1,59 @@
+extends Actor
+
+var jumped_once: bool
+export var stomp_impulse: = 1000.0
+
+func _on_EnemyDetector_area_entered(area):
+	_velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
+
+func _on_EnemyDetector_body_entered(body):
+	queue_free()
+
+	
+func _physics_process(delta: float) -> void:
+	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
+	var direction: = get_direction()
+	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
+	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+
+
+func get_direction() -> Vector2:
+		var jump: bool
+		if is_on_floor() and Input.is_action_just_pressed("jump"):
+			jumped_once = true
+			jump = true
+		else: 
+			jump = false
+			if jumped_once == true and Input.is_action_just_pressed("jump"):
+				jumped_once = false
+				jump = true
+			
+
+			
+	
+		return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), 
+		-Input.get_action_strength("jump") if jump
+		else 0.0
+	)
+
+
+func calculate_move_velocity(
+		linear_velocity: Vector2,
+		direction: Vector2,
+		speed: Vector2,
+		is_jump_interrupted: bool
+	) -> Vector2:
+	var out: = linear_velocity
+	out.x = speed.x * direction.x
+	out.y += gravity * get_physics_process_delta_time()
+	if direction.y == -1.0:
+		out.y = speed.y * direction.y
+	if is_jump_interrupted:
+		out.y = 0.0
+	return out
+
+func calculate_stomp_velocity(linear_velocity: Vector2, impulse: float) -> Vector2:
+	var out: = linear_velocity
+	out.y = -impulse
+	return out
